b35b3a9987faff3808343140ea1f1fb0aa8adce4
Eduardo Rocha Azevedo <142415927+Eduardo-Rocha-Azevedo@users.noreply.github.com>
2023-11-14 10:10:28 -0300

    metodos iniciado

---
+++ Aula.java
@@ -0,0 +1,8 @@
+public class Aula {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int[] num = new int[5];
+
+        System.out.printf("%d",num[0]);
+    }
+}
---
+++ Vetor.java
@@ -0,0 +1,12 @@
+public class Vetor {
+    public static void main(String[] args) {
+        int[] num = new int[5];
+        num[0] = 10;
+
+        int[] numeros= {10,20,30,40,50};
+        for(int i=0; i<numeros.length; i++){
+            System.out.printf("%d\n",numeros[i]);
+        }
+
+    }
+}
